7a7911d84e8d8ff59c3bbbd49aa70bd6f8c542e2
Bonnie Schulkin <biz@bonnieschulkin.com>
2021-12-01 20:17:14 -0800

    Initial commit

---
+++ backup.sh
@@ -0,0 +1,43 @@
+# usage: bash backup.sh /path/to/directory/with/screenflow/files
+
+cmd="tar"
+# cmd="echo tar"
+
+export cmd
+
+datestring=`date +%s`
+dest="raw screenflow zipped"
+
+echo "RENAMING old directory"
+mv -f "$1/${dest}" "$1/${dest} ${datestring}"
+
+echo "CREATING new directory"
+mkdir "$1/raw screenflow zipped"
+
+for dir in "" "_archived" "_unused"
+do
+  echo "PROCESSING $1/${dir}"
+  for sfile in "$1/${dir}"/*.screenflow ; do
+    f="${sfile##*/}"
+    echo "    ${f}"
+    ${cmd} -czf "$1/${dest}/${dir}/${f}.tar.gz" -C "$1/${dir}" "${f}" 
+  done  
+done
+
+# echo "PROCESSING $1"
+# dir=$1
+# for sfile in "$1"/*.screenflow ; do
+#   f="${sfile##*/}"
+#   ${cmd} -czf "$1/${dest}/${f}.tar.gz" "$1/${f}" 
+# done
+
+# echo "PROCESSING $1/_archived"
+# for sfile in "$1"/_archived/*.screenflow ; do
+#   f="${sfile##*/}"
+#   ${cmd} -czf "$1/${dest}/${f}.tar.gz" "$1/${f}" 
+# done
+
+# echo "PROCESSING $1/_unused"
+#   f="${sfile##*/}"
+#   ${cmd} -czf "$1/${dest}/${f}.tar.gz" "$1/${f}" 
+# done
